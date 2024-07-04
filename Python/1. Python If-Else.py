14bb5d6fa0f941ac9e77d8f023a2cff22f1615bc
Bilal Khan <khn.bilal17@gmail.com>
2024-07-05 00:17:27 +0530

    Create  1. Python If-Else.py

---
+++ Python/1. Python If-Else.py
@@ -0,0 +1,18 @@
+# Problem Statement: https://www.hackerrank.com/challenges/py-if-else/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+
+n = int(input().strip())
+if n%2 != 0:
+        print("Weird")
+elif n%2 == 0 and n>2 and n<=5:
+    print("Not Weird")
+elif n%2 ==0 and n > 6 and n <=20:
+    print("Weird")
+else:
+    print("Not Weird")
