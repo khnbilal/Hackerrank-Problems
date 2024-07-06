f0f9a8b34cb63f7625b8d00f8e5b9971a2dbd8d5
Bilal Khan <khn.bilal17@gmail.com>
2024-07-06 20:42:43 +0530

    Create 13. Weather Observation Station 9.sql

---
+++ SQL/13. Weather Observation Station 9.sql
@@ -0,0 +1,12 @@
+-- Problem Statement: https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT LIKE 'A%'
+  AND CITY NOT LIKE 'E%'
+  AND CITY NOT LIKE 'I%'
+  AND CITY NOT LIKE 'O%'
+  AND CITY NOT LIKE 'U%'
+ORDER BY CITY;
+
+
