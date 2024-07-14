cc88a1aef26bffdf77788927653c305615eef585
Bilal Khan <khn.bilal17@gmail.com>
2024-07-14 08:30:39 +0530

    Create 5. Print Function.py

---
+++ Python/5. Print Function.py
@@ -0,0 +1,7 @@
+# Problem Statement: https://www.hackerrank.com/challenges/python-print/problem
+
+n = int(input())
+for i in range(1, n + 1):
+    print(i, end="")
+
+# I have used the `end=""` function to print each number without a newline. 
